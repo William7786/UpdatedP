69584edf89b6d2a55b5665e0023fd6c244ac23a1
Tcomp <williamt7786@gmail.com>
2022-12-19 22:51:51 -0500

    For website assets

---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'William Fyle',
+    title: "Web Developer",
+    desciption:"Below are some of my academic achievements in my pursuit of personal development. gdsfdsfdsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsd",
+    image: '',
+    resumePdf: resume
+}
