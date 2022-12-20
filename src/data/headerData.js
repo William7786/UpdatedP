2d2d16c4b49e59e6f17cc22a3ca487c21ba319e9
Tcomp <williamt7786@gmail.com>
2022-12-19 23:54:32 -0500

    For Header Data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
+import image from '../assets/img/3.jpeg'
 
 export const headerData = {
     name: 'William Fyle',
     title: "Web Developer",
-    desciption:"I have not failed i've just found ten thousand ways it wont work",
-    image: '',
+    desciption:"If I fail, i try again, and again, and again. It matters not how you fail but how you learn from it. You will always pass failure on your way to success ",
+    image: image,
     resumePdf: resume
 }
