fc76d0ed4c6152abd1a0a923107ebdf85a21b2b8
Tcomp <williamt7786@gmail.com>
2022-12-19 23:08:26 -0500

    For general information changes

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,22 +1,22 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Ramjas College, Delhi University',
-        course: 'B.A. Honours History',
-        startYear: '2015',
-        endYear: '2018'
+        institution: 'East Carolina University',
+        course: 'Business Administration',
+        startYear: '2018',
+        endYear: '2022'
     },
     {
         id: 2,
-        institution: 'Delhi University',
-        course: 'M.A. History',
-        startYear: '2019',
+        institution: 'Unc Chapel Hill',
+        course: 'Full Stack Web Development ',
+        startYear: '2020',
         endYear: '2021'
     },
     {
         id: 3,
-        institution: 'Masai School of Coding',
-        course: 'Fullstack Web-Development',
+        institution: 'Nc State',
+        course: 'Digital Marketing',
         startYear: '2021',
         endYear: 'Present'
     },
--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,13 +1,13 @@
 export const socialsData = {
-    github: 'https://github.com/sreerag-rajan',
+    github: 'https://github.com/William7786',
     facebook: '',
-    linkedIn: 'https://www.linkedin.com/in/sreerag-rajan-55383a222',
+    linkedIn: 'https://www.linkedin.com/in/williamf7786/',
     instagram: '',
     codepen: '',
     twitter: '',
     reddit: '',
     blogger: '',
-    medium: 'https://medium.com/@sreerag.rajan5',
+    medium: '',
     stackOverflow: '',
     gitlab: '',
     youtube: ''
